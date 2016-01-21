246c8239165b844b6416a47aad241a38a938415d
brooke hurford and jackson cafazzo <brookebhurford@gmail.com>
2016-01-21 11:07:55 -0800

    Add html and css, remove 0 from output

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 var countUpBy = function(countTo, countBy) {
   var finalResult = [];
 
-  for (var i = 0; i <= (countTo); i += countBy) {
+  for (var i = 0; i <= countTo; i += countBy) {
+    if (i !== 0) {
     finalResult.push(i);
+    }
   }
   return finalResult
 }
 
+$(document).ready(function(){
+  $("form#countUpBy").submit(function(event) {
+    var countTo = parseInt($("input#countTo").val());
+    var countBy = parseInt($("input#countBy").val());
+    var numberString = countUpBy(countTo, countBy);
 
-
-
-// var countUpBy = function(countTo, countBy) {
-//   var finalResult = '';
-//
-//   for (var i = 0; i < countTo; i += countBy) {
-//     var result = new Set();
-//     result.add(i + ", ");
-//     finalResult.concat(result);
-//   }
-//   return finalResult
-// }
+    $(".numberString").text(numberString);
+    $(".result").show();
+    event.preventDefault();
+  });
+});
